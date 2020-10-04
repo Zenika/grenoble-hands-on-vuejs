e006f12de254ab6f2323cf2b9f5b2eca9037535f
Jules H <jules.hablot@zenika.com>
2020-10-04 17:53:27 +0200

    create new method to have only first day weather and present response field

--- src/api/weather.api.js
+++ src/api/weather.api.js
@@ -1,5 +1,19 @@
 export default {
-  getCityWeather(long, lat) {
-    return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`).then(response => response.json())
+  getCityNextWeekWeather(long, lat) {
+    return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
+      .then(response => response.json())
+      .then(response => response.dataseries)
+  },
+  getCityTodayWeather(long, lat) {
+    return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
+      .then(response => response.json())
+      .then(response => response.dataseries)
+      .then(dataseries => dataseries[0])
+      // date: 20201004
+      // temp2m: {max: 31, min: 26}
+      // max: 31
+      // min: 26
+      // weather: "ishower"
+      // wind10m_max: 3
   }
 }
