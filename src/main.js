606a5efc051bea209e52e107ab297be503baa7f9
Martin Choraine <martin.choraine@gmail.com>
2020-10-04 10:03:10 +0200

    💄 add bulma for style

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import 'bulma/css/bulma.min.css'
 
 createApp(App)
   .use(store)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <section>
-    <h2>
-      Offices
-    </h2>
-    <nav>
-      <router-link :to="{ name: 'City', params: { cityName: cities[0]}}">
-        <p>
-          {{ cities[0] }}
-        </p>
+    <h1 class="title">Offices</h1>
+
+    <div class="panel">
+      <router-link class="panel-block p-4" :to="{ name: 'City', params: { cityName: cities[0]}}">
+        <h2 class="subtitle">{{ cities[0] }}</h2>
       </router-link>
-    </nav>
+    </div>
   </section>
 </template>
 <script>
