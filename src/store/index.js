edcf1ad9b86aafb55ac6c6692cccc5347f3cb8cd
Hadrien THOMAS <hadrien.thomas@zenika.com>
2020-10-14 08:08:32 +0200

    refacto(store): positions & cities refacto

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,63 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
   state: {
-    latLongMap: {
-      GRENOBLE: [45.183916, 5.703630],
-      SINGAPOUR: [1.295600, 103.858995],
-      BORDEAUX: [44.848089, -0.571017],
-      BREST: [48.389397, -4.499237],
-      MONTREAL: [45.523000, -73.581700],
-      LYON: [45.767443, 4.858798],
-      RENNES: [48.113409, -1.661249],
-      NANTES: [47.207408, -1.556187],
-      LILLE: [50.648670, 3.075520],
-      PARIS: [48.878932, 2.328487]
-    }
+    cities: [
+      {
+        name: 'Grenoble',
+        latitude: 45.183916,
+        longitude: 5.703630
+      },
+      {
+        name: 'Singapour',
+        latitude: 1.295600,
+        longitude: 103.858995
+      },
+      {
+        name: 'Bordeaux',
+        latitude: 44.848089,
+        longitude: -0.571017
+      },
+      {
+        name: 'Brest',
+        latitude: 48.389397,
+        longitude: -4.499237
+      },
+      {
+        name: 'Montréal',
+        latitude: 45.523000,
+        longitude: -73.581700
+      },
+      {
+        name: 'Lyon',
+        latitude: 45.767443,
+        longitude: 4.858798
+      },
+      {
+        name: 'Rennes',
+        latitude: 48.113409,
+        longitude: -1.661249
+      },
+      {
+        name: 'Nantes',
+        latitude: 47.207408,
+        longitude: -1.556187
+      },
+      {
+        name: 'Lille',
+        latitude: 50.648670,
+        longitude: 3.075520
+      },
+      {
+        name: 'Paris',
+        latitude: 48.878932,
+        longitude: 2.328487
+      },
+    ]
   },
   getters: {
-    getMapPosition: state => state.latLongMap,
-    getCityPosition: (state, getters) => cityName => getters.getMapPosition[cityName],
-    getCities: state => Object.keys(state.latLongMap),
+    getCities: state => state.cities,
+    getCityByName: (state, getters) => cityName => getters.getCities.find(city => city.name === cityName),
   },
   mutations: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <h1 class="title">Offices</h1>
 
     <div class="panel">
-      <router-link class="panel-block p-4" :to="{ name: 'City', params: { cityName: cities[0]}}">
-        <h2 class="subtitle">{{ cities[0] }}</h2>
+      <router-link class="panel-block p-4" :to="{ name: 'City', params: { cityName: cities[0].name}}">
+        <h2 class="subtitle">{{ cities[0].name }}</h2>
       </router-link>
     </div>
   </section>
