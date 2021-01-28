ba3f6fb60a978bd57e7415a806465d80555f5c19
AGACK Moustapha <moustapha.agack@zenika.com>
2021-01-28 18:00:21 +0000

    Setup Cypress (#24)
    * wip
    
    * feat: setup Cypress with single tests
    
    Co-authored-by: Moussto <inhaeden@gmail.com>

---
+++ cypress/integration/examples/mine.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+context('City creation', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+  })
+
+  it('should properly add a city', () => {
+    // Given a big enough viewport
+
+    cy.viewport(1200, 1200)
+    // When swithcing to new city page
+    cy.get('.navbar-end > .navbar-item')
+      .click()
+
+    // and adding new city
+    cy.get(':nth-child(1) > .control > .input').type('Niamey')
+    cy.get(':nth-child(2) > .control > .input').type('13.5045')
+    cy.get(':nth-child(3) > .control > .input').type('2.1065')
+    cy.get('.button').click()
+
+    // Then im redirected to main page, and city is added
+    cy.get('[href="/Niamey"]').should('be.visible', 'Niamey')
+  });
+
+
+})
