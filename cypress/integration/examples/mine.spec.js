32cfc791b7ba3bff58317d6760031fd9013565d0
Moussto <inhaeden@gmail.com>
2021-01-28 18:41:48 +0100

    feat: setup Cypress with single tests

--- cypress/integration/examples/mine.spec.js
+++ cypress/integration/examples/mine.spec.js
@@ -23,23 +23,5 @@ context('City creation', () => {
     cy.get('[href="/Niamey"]').should('be.visible', 'Niamey')
   });
 
-  it('should properly add a city', () => {
-    // Given a big enough viewport
-
-    cy.viewport(1200, 1200)
-    // When swithcing to new city page
-    cy.get('.navbar-end > .navbar-item')
-      .click()
-
-    // and adding new city
-    cy.get(':nth-child(1) > .control > .input').type('Niamey')
-    cy.get(':nth-child(2) > .control > .input').type('13.5045')
-    cy.get(':nth-child(3) > .control > .input').type('2.1065')
-    cy.get('.button').click()
-
-    // Then im redirected to main page, and city is added
-    cy.get('[href="/Niamey"]').should('be.visible', 'Niamey')
-  });
-
 
 })
