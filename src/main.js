a8a9b2f00f3141dc62fedcc3688e8639e5afc130
Jules Hablot <jules.hablot@zenika.com>
2021-02-03 18:51:38 +0100

    add vue axe (#26)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
+import {createApp, h, Fragment} from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 import 'bulma/css/bulma.min.css'
 
-createApp(App)
+import VueAxe, {VueAxePopup} from "vue-axe";
+
+createApp({
+  render: () => h(Fragment, [h(App), h(VueAxePopup)])
+}).use(VueAxe)
   .use(store)
   .use(router)
   .mount('#app')
