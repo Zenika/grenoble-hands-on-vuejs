b6d6ce94d7ae741499e48813ae896397dafb27a3
Jules Hablot <p0ppoff@MacBook-Pro-de-Jules.local>
2022-02-09 12:18:22 +0100

    set new Grenika location

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     latLongMap: {
-      GRENOBLE: [45.183916, 5.703630],
+      GRENOBLE: [45.189397, 5.722261],
       SINGAPOUR: [1.295600, 103.858995],
       BORDEAUX: [44.848089, -0.571017],
       BREST: [48.389397, -4.499237],
