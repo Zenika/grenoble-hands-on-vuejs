fcbb723f484069998c2e85b99f75b8b543a17083
Arno <arno.tgh@gmail.com>
2023-10-19 11:40:03 +0200

    api refacto

--- src/api/weather.api.js
+++ src/api/weather.api.js
@@ -1,13 +1,11 @@
-export default {
-  getCityNextWeekWeather(long, lat) {
-    return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
-      .then(response => response.json())
-      .then(response => response.dataseries)
-  },
-  getCityTodayWeather(long, lat) {
-    return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
-      .then(response => response.json())
-      .then(response => response.dataseries)
-      .then(dataseries => dataseries[0])
-  }
+export const getCityNextWeekWeather = (long, lat) => {
+  return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
+    .then(response => response.json())
+    .then(response => response.dataseries)
+}
+export const getCityTodayWeather = (long, lat) => {
+  return fetch(`http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`)
+    .then(response => response.json())
+    .then(response => response.dataseries)
+    .then(dataseries => dataseries[0])
 }
